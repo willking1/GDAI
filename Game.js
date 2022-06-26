fbf87dd739de014027b576111aca80d95938ddc6
Max <max.rochnik@gmail.com>
2022-06-26 00:22:22 -0700

    player object collision

--- Game.js
+++ Game.js
@@ -26,16 +26,34 @@ class Game {
     }
 
     move() {
-        if(this.player.speed != 0) {
+        console.log(this.isTouchingGround());
+        if(!this.isTouchingGround()) {
             this.player.speed -= 0.05;
-            if(this.player.speed+0.05 > -5 && this.player.speed-0.05 < -5) {
-                this.player.speed = 0;
-            } 
+        } else if(this.player.speed < 0) {
+            this.player.speed = 0;
         }
         this.player.y+=this.player.speed;
         for(let i=0; i<this.objects.length; i++) {
             this.objects[i].x--;
         }
+    }
+
+    isTouchingGround() {
+        //check if player is touching any objects
+        for(let i=0; i<this.objects.length; i++) {
+            //check if player x position aligns with any object x positions
+            if((this.player.x > this.objects[i].x && this.player.x < this.objects[i].x+this.objects[i].width) || (this.player.x+this.player.width > this.objects[i].x && this.player.x+this.player.width < this.objects[i].x+this.objects[i].width)) {
+                //check if player y is at same height as block
+                if(this.player.y <= 0.05+this.objects[i].y+this.objects[i].height) {
+                    return true;
+                }
+            }
+        }
+        //check if player is touching main ground level
+        if(this.player.y <= 0.05) {
+            return true;
+        } 
+        return false;
     }
 
     map() {
